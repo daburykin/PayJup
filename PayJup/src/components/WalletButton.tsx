85ea3380d86c9c62bd8713f98f15e277e929c5c5
Daniil <burykindandy322@gmail.com>
2025-02-27 17:11:04 +0100

    Delete PayJup directory

--- PayJup/src/components/WalletButton.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import dynamic from 'next/dynamic';
-
-export const WalletButton = dynamic(
-  () => Promise.resolve(WalletMultiButton),
-  { ssr: false }
-); 
